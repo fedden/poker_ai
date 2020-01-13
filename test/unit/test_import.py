26eed37d841c642b791f97b2705f75153ae1690b
leonfedden <leonfedden@gmail.com>
2020-01-13 21:32:43 +0000

    adding the test dir with a basic import test

---
+++ test/unit/test_import.py
@@ -0,0 +1,8 @@
+def test_import():
+    """Test the imports work"""
+    import pluribus
+    from pluribus import ai, game
+    from pluribus.ai import pluribus
+    from pluribus.game import actions, card, dealer, deck, hand, player
+    from pluribus.game import state, table, evaluation
+    from pluribus.game.evaluation import eval_card, evaluator, lookup
