bdac26c6dceeb39ac93fc64433be3d36b2527a7a
leonfedden <leonfedden@gmail.com>
2020-01-15 23:06:09 +0000

    add seed to functional test

--- test/functional/test_engine.py
+++ test/functional/test_engine.py
@@ -4,10 +4,12 @@ import pytest
 @pytest.mark.parametrize("n_players", [2, 4, 6])
 def test_hand(n_players):
     """Test a hand can be played."""
+    from pluribus import utils
     from pluribus.ai.dummy import RandomPlayer
     from pluribus.game.table import PokerTable
     from pluribus.game.engine import PokerEngine
     from pluribus.game.pot import Pot
+    utils.random.seed(42)
     initial_chips_amount = 10000
     small_blind_amount = 10
     big_blind_amount = 50
