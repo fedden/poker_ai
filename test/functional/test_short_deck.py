e8a5d6ffe50e31e586166e9cbbb1bb1169bdab01
leonfedden <leonfedden@gmail.com>
2020-03-22 21:56:02 +0000

    add unfinished test

---
+++ test/functional/test_short_deck.py
@@ -0,0 +1,24 @@
+def test_short_deck():
+    """Test the short deck poker game state works as expected."""
+    from pluribus.games.short_deck.player import ShortDeckPokerPlayer
+    from pluribus.games.short_deck.state import ShortDeckPokerState
+    from pluribus.poker.pot import Pot
+
+    n_players = 3
+    pot = Pot()
+    players = [
+        ShortDeckPokerPlayer(player_i=player_i, pot=pot, initial_chips=10000)
+        for player_i in range(n_players)
+    ]
+    state = ShortDeckPokerState(players=players)
+    # Call for all players.
+    for player_i in range(n_players):
+        assert state.current_player.name == f"player_{player_i}"
+        assert len(state.legal_actions) == 3
+        state = state.apply_action(action_str="call")
+    import ipdb
+
+    ipdb.set_trace()
+
+
+test_short_deck()
