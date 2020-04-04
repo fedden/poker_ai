98c88b130ad49f3826e35fe3cdb6edc45f15f4b5
leonfedden <leonfedden@gmail.com>
2020-04-04 20:34:05 +0100

    iterating on run.py

--- applications/visualisation/run.py
+++ applications/visualisation/run.py
@@ -1,7 +1,6 @@
-import random
 import requests
 
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__, static_folder="./dist/static", template_folder="./dist")
 
