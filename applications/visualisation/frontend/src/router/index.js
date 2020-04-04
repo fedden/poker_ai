1b38242828f77c6c7c411f81d3ee4883653d2456
leonfedden <leonfedden@gmail.com>
2020-04-04 20:16:37 +0100

    remove unneeded code

--- applications/visualisation/frontend/src/router/index.js
+++ applications/visualisation/frontend/src/router/index.js
@@ -3,7 +3,8 @@ import Router from 'vue-router'
 
 const routerOptions = [
   { path: '/', component: 'Home' },
-  { path: '/about', component: 'About' }
+  { path: '/about', component: 'About' },
+  { path: '*', component: 'NotFound' }
 ]
 
 const routes = routerOptions.map(route => {
--- applications/visualisation/run.py
+++ applications/visualisation/run.py
@@ -2,16 +2,8 @@ import random
 import requests
 
 from flask import Flask, render_template, jsonify
-from flask_cors import CORS
 
 app = Flask(__name__, static_folder="./dist/static", template_folder="./dist")
-cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
-
-
-@app.route("/api/random")
-def random_number():
-    response = {"randomNumber": random.randint(1, 100)}
-    return jsonify(response)
 
 
 @app.route("/", defaults={"path": ""})
