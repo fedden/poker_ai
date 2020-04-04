bf8f82730fdc60b6b3b7005bd940e1332521368d
leonfedden <leonfedden@gmail.com>
2020-04-04 23:11:56 +0100

    got the formatter/fixers working correctly

--- applications/visualisation/frontend/.eslintrc.js
+++ applications/visualisation/frontend/.eslintrc.js
@@ -1,29 +1,38 @@
-// https://eslint.org/docs/user-guide/configuring
-
 module.exports = {
-  root: true,
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
   env: {
     browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
   },
-  extends: [
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
-    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-    'standard'
-  ],
-  // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
-  // add your custom rules here
+  extends: ["eslint:recommended", "prettier"],
+  plugins: ["prettier"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    sourceType: "module",
+  },
   rules: {
-    // allow async-await
-    'generator-star-spacing': 'off',
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  }
-}
+    "no-const-assign": "warn",
+    "no-this-before-super": "warn",
+    "no-undef": "warn",
+    "no-unreachable": "warn",
+    "no-unused-vars": "warn",
+    "constructor-super": "warn",
+    "valid-typeof": "warn",
+    "prettier/prettier": [
+      "error",
+      {
+        trailingComma: "es5",
+        singleQuote: true,
+        printWidth: 120,
+        semi: false,
+      },
+    ],
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "single"],
+    semi: ["error", "never"],
+  },
+};
--- applications/visualisation/frontend/src/main.js
+++ applications/visualisation/frontend/src/main.js
@@ -12,70 +12,69 @@ Vue.config.productionTip = false
 // })
 
 Vue.component('card', {
-    template: `<div class="card" :class="['figures-' + card.f, 'values-' + card.v]">
+  template: `<div class='card' :class='['figures-' + card.f, 'values-' + card.v]'>
         <h1>{{card.v}}</h1>
-        <div class="figures" :class="card.f"></div>
+        <div class='figures' :class='card.f'></div>
         <h1>{{card.v}}</h1>
     </div>`,
-    props: ['card']
+  props: ['card'],
 })
 
-let app = new Vue({
-    el: '.vue-container',
-    data: {
-        player_playing: 0,
-        players: [
-            {name:'rivy33', bank: 100, onTable: 77, hasCards: false},
-            {name:'kattar', color: 'cyan', bank: 400, onTable: 300, hasCards: true},
-            {name:'mikelaire', color: 'lightcoral', bank: 100, onTable: 20, hasCards: false},
-            {name:'tomtom', color: 'crimson', bank: 100, onTable: 20, hasCards: false},
-            {name:'nana', color: '#444', bank: 100, onTable: 20, hasCards: false},
-            // {name:'ionion', color: 'forestgreen', bank: 100, onTable: 20, hasCards: false},
-            // {name:'link6996', color: 'goldenrod', bank: 100, onTable: 20, hasCards: false},
-            // {name:'gossboganon', color: 'gold', bank: 100, onTable: 20, hasCards: false}
-        ],
-        figures : [
-            'P',
-            'H',
-            'C',
-            'D'
-        ],
-        values : [
-            '2',
-            '3',
-            '4',
-            '5',
-            '6',
-            '7',
-            '8',
-            '9',
-            '10',
-            'J',
-            'Q',
-            'K',
-            'A',
-        ]
-    },
-    computed: {
-        cards () {
-            let all = []
-            for (let figure of this.figures) {
-                for (let value of this.values) {
-                    all.push({
-                        f: figure,
-                        v: value
-                    })
-                }
-            }
-            return all
-        },
-        five_cards () {
-            let fives = []
-            for (let i = 0; i < 5; i++) {
-                let rand_id = parseInt(Math.random() * this.cards.length)
-                fives.push(this.cards[rand_id])
-            }
-            return fives
+new Vue({
+  el: '.vue-container',
+  data: {
+    player_playing: 0,
+    players: [
+      { name: 'rivy33', bank: 100, onTable: 77, hasCards: false },
+      {
+        name: 'kattar',
+        color: 'cyan',
+        bank: 400,
+        onTable: 300,
+        hasCards: true,
+      },
+      {
+        name: 'mikelaire',
+        color: 'lightcoral',
+        bank: 100,
+        onTable: 20,
+        hasCards: false,
+      },
+      {
+        name: 'tomtom',
+        color: 'crimson',
+        bank: 100,
+        onTable: 20,
+        hasCards: false,
+      },
+      { name: 'nana', color: '#444', bank: 100, onTable: 20, hasCards: false },
+      // {name:'ionion', color: 'forestgreen', bank: 100, onTable: 20, hasCards: false},
+      // {name:'link6996', color: 'goldenrod', bank: 100, onTable: 20, hasCards: false},
+      // {name:'gossboganon', color: 'gold', bank: 100, onTable: 20, hasCards: false}
+    ],
+    figures: ['P', 'H', 'C', 'D'],
+    values: ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'],
+  },
+  computed: {
+    cards() {
+      let all = []
+      for (let figure of this.figures) {
+        for (let value of this.values) {
+          all.push({
+            f: figure,
+            v: value,
+          })
         }
-    }
+      }
+      return all
+    },
+    five_cards() {
+      let fives = []
+      for (let i = 0; i < 5; i++) {
+        let rand_id = parseInt(Math.random() * this.cards.length)
+        fives.push(this.cards[rand_id])
+      }
+      return fives
+    },
+  },
 })
