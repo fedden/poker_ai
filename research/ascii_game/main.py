12665e4f9bd244cdaeee41d365242b047179c240
leonfedden <leonfedden@gmail.com>
2020-05-14 11:02:30 +0100

    hopefully printing winner

--- research/ascii_game/main.py
+++ research/ascii_game/main.py
@@ -1,6 +1,7 @@
 import copy
 import random
-from typing import Dict, List
+from operator import itemgetter
+from typing import Any, Dict, List
 
 import joblib
 import numpy as np
@@ -11,22 +12,25 @@ from card_collection import CardCollection
 from player import Player
 
 
-def _compute_header_str(state: ShortDeckPokerState) -> str:
+def _compute_header_str(
+    state: ShortDeckPokerState, player_names: str, table_rotation: int
+) -> str:
     if state.is_terminal:
-        winner_i = None
-        best_delta = -np.inf
+        player_winnings = []
         for player_i, chips_delta in state.payout.items():
-            if chips_delta > best_delta:
-                best_delta = chips_delta
-                winner_i = player_i
-        winner = state.players[winner_i]
-        header_str = f"{state.betting_stage} - {winner.name} wins {best_delta} chips"
+            rotated_i = (player_i + table_rotation) % len(state.players)
+            player_winnings.append((player_names[rotated_i], chips_delta))
+        player_winnings.sort(key=itemgetter(1))
+        strings = [
+            f"{n} {'wins' if x > 0 else 'loses'} {x} chips" for n, x in player_winnings
+        ]
+        header_str = f"{state.betting_stage} - {', '.join(strings)}"
     else:
         header_str = state.betting_stage
     return header_str
 
 
-def print_header(state: ShortDeckPokerState):
+def print_header(state: ShortDeckPokerState, player_names: str, table_rotation: int):
     header_str = _compute_header_str(state)
     print(term.center(term.yellow(header_str)))
     print()
@@ -56,6 +60,12 @@ def print_table(players: List[Player], public_cards: CardCollection):
         print(term.center(line))
 
 
+def rotate(l: List[Any], rotate_amount: int):
+    if rotate_amount > len(l):
+        raise ValueError
+    return l[rotate_amount:] + l[:rotate_amount]
+
+
 term = Terminal()
 
 table_position_to_orientation: Dict[int, str] = {0: "top", 1: "right", 2: "bottom"}
@@ -63,7 +73,9 @@ n_players: int = 3
 pickle_dir: str = "/home/tollie/dev/pluribus/research/blueprint_algo"
 state: ShortDeckPokerState = new_game(n_players, pickle_dir=pickle_dir)
 is_bot: List[bool] = [False, True, True]
+player_names: List[str] = ["you", "bot 1", "bot 2"]
 selected_action_i: int = 0
+table_rotation: int = 0
 agent: str = "offline"
 strategy_path: str = (
     "/home/tollie/dev/pluribus/research/blueprint_algo/offline_strategy_285800.gz"
@@ -72,11 +84,12 @@ if agent in {"offline", "online"}:
     offline_strategy = joblib.load(strategy_path)
 with term.cbreak(), term.hidden_cursor():
     while True:
+        # Construct ascii objects to be rendered later.
         players: List[Player] = []
-        for player_i, state_player in enumerate(state.players):
+        for player_i, state_player in enumerate(rotate(state.players, table_rotation)):
             ascii_player = Player(
                 *state_player.cards,
-                name=state_player.name,
+                name=player_names[player_i],
                 term=term,
                 info_position=table_position_to_orientation[player_i],
                 hide_cards=is_bot[player_i] and not state.is_terminal,
@@ -94,10 +107,12 @@ with term.cbreak(), term.hidden_cursor():
             legal_actions = ["quit", "new game"]
         else:
             legal_actions = [] if is_bot[state.player_i] else state.legal_actions
+        # Render game.
         print(term.home + term.white + term.clear)
-        print_header(state)
+        print_header(state, player_names, table_rotation)
         print_table(players, public_cards)
         print_footer(selected_action_i, legal_actions)
+        # Make action of some kind.
         if not is_bot[state.player_i] or state.is_terminal:
             key = term.inkey(timeout=None)
             if key.name == "KEY_LEFT":
@@ -115,6 +130,7 @@ with term.cbreak(), term.hidden_cursor():
                     state: ShortDeckPokerState = new_game(
                         n_players, state.info_set_lut,
                     )
+                    table_rotation = (table_rotation + 1) % n_players
                 else:
                     state: ShortDeckPokerState = state.apply_action(action)
         else:
