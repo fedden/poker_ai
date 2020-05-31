63f99e5bb85ac79d0000bdf01cc538c719528e2b
leonfedden <leonfedden@gmail.com>
2020-05-31 16:43:54 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -4,19 +4,19 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pluribus-fedden",  # Replace with your own username
-    version="0.0.1",
-    author="Leon Fedden",
+    name="poker_ai",
+    version="1.0.0",
+    author="Leon Fedden, Colin Manko",
     author_email="leonfedden@gmail.com",
-    description="Open source implementation of the pluribus poker AI ployer.",
+    description="Open source implementation of a CFR based poker AI player.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/fedden/pluribus",
+    url="https://github.com/fedden/poker_ai",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.7",
 )
