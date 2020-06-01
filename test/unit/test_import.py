60bc57de607c1e2117a136d426c41fb6ef67bef1
leonfedden <leonfedden@gmail.com>
2020-06-01 09:31:45 +0100

    fix import test

--- test/unit/test_import.py
+++ test/unit/test_import.py
@@ -2,7 +2,8 @@ def test_import():
     """Test the imports work"""
     import poker_ai
     from poker_ai import ai, poker
-    from poker_ai.ai import pluribus
+    from poker_ai.ai import runner
+    from poker_ai.terminal import runner
     from poker_ai.games.short_deck import player, state
     from poker_ai.poker import actions, card, dealer, deck, engine, player
     from poker_ai.poker import state, table, evaluation
