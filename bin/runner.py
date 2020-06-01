dbb15867dc94e3cbc4c89d107bf50c4f12807e1f
leonfedden <leonfedden@gmail.com>
2020-06-01 08:50:34 +0100

    getting local scripts

---
+++ bin/runner.py
@@ -0,0 +1,6 @@
+import click
+
+
+def cli():
+    """The CLI for the poker_ai package."""
+    pass
--- setup.py
+++ setup.py
@@ -1,7 +1,21 @@
+import glob
 import setuptools
+from typing import List
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+
+def get_scripts_from_bin() -> List[str]:
+    """Get all local scripts from bin so they are included in the package."""
+    return glob.glob("bin/*")
+
+
+def get_package_description() -> str:
+    """Returns a description of this package from the markdown files."""
+    with open("README.md", "r") as stream:
+        readme: str = stream.read()
+    with open("HISTORY.md", "r") as stream:
+        history: str = stream.read()
+    return f"{readme}\n\n{history}"
+
 
 setuptools.setup(
     name="poker_ai",
@@ -9,14 +23,15 @@ setuptools.setup(
     author="Leon Fedden, Colin Manko",
     author_email="leonfedden@gmail.com",
     description="Open source implementation of a CFR based poker AI player.",
-    long_description=long_description,
+    long_description=get_package_description(),
     long_description_content_type="text/markdown",
     url="https://github.com/fedden/poker_ai",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GPL License",
         "Operating System :: OS Independent",
     ],
+    scripts=get_scripts_from_bin(),
     python_requires=">=3.7",
 )
