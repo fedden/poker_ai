75fc9caf22d6ba337e4ea9ddad2ec5607c0ecf17
big-c-note <colin@colinmanko.com>
2020-07-04 01:07:39 -0400

    new dealer test

--- poker_ai/clustering/preflop.py
+++ poker_ai/clustering/preflop.py
@@ -5,39 +5,63 @@ import math
 from poker_ai.poker.card import Card
 
 
-def nCr(n: int, r: int):
-    """Combinations helper: n choose r."""
-    if n < r:
-        # Following the convention that if n < r, return 0
+def make_starting_hand_lossless(starting_hand, short_deck) -> int:
+    """"""
+    ranks = []
+    suits = []
+    for card in starting_hand:
+        ranks.append(card.rank_int)
+        suits.append(card.suit)
+    if len(set(suits)) == 1:
+        suited = True
+    else:
+        suited = False
+    if all(c_rank == 14 for c_rank in ranks):
         return 0
-    f = math.factorial
-    return f(n) / f(r) / f(n-r)
+    elif all(c_rank == 13 for c_rank in ranks):
+        return 1
+    elif all(c_rank == 12 for c_rank in ranks):
+        return 2
+    elif all(c_rank == 11 for c_rank in ranks):
+        return 3
+    elif all(c_rank == 10 for c_rank in ranks):
+        return 4
+    elif 14 in ranks and 13 in ranks:
+        return 5 if suited else 15
+    elif 14 in ranks and 12 in ranks:
+        return 6 if suited else 16
+    elif 14 in ranks and 11 in ranks:
+        return 7 if suited else 17
+    elif 14 in ranks and 10 in ranks:
+        return 8 if suited else 18
+    elif 13 in ranks and 12 in ranks:
+        return 9 if suited else 19
+    elif 13 in ranks and 11 in ranks:
+        return 10 if suited else 20
+    elif 13 in ranks and 10 in ranks:
+        return 11 if suited else 21
+    elif 12 in ranks and 11 in ranks:
+        return 12 if suited else 22
+    elif 12 in ranks and 10 in ranks:
+        return 13 if suited else 23
+    elif 11 in ranks and 10 in ranks:
+        return 14 if suited else 24
 
 
-def rank_set(cards: List[Card], idx=0):
-    """Recursive function for gicing unique id to hoe cards."""
-    if len(cards) == 1:
-        # Offsetting by 2 since ranks go from 2-14.
-        return int(idx + cards[0].rank_int - 2)
-    # Offsetting by 2 since ranks go from 2-14.
-    idx += nCr(cards[0].rank_int - 2, len(cards))
-    return rank_set(cards=cards[1:], idx=idx)
-
-
-def compute_preflop_lossless_abstraction(builder) -> Dict[Tuple[int, int], int]:
+def compute_preflop_lossless_abstraction(builder) -> Dict[Tuple[Card, Card], int]:
     """Compute the preflop abstraction dictionary.
 
     Only works for the short deck presently.
     """
     # Making sure this is 20 card deck with 2-9 removed
-#    allowed_ranks = {10, 11, 12, 13, 14}
- #   found_ranks = set([c.rank_int for c in builder._cards])
-  #  if found_ranks != allowed_ranks:
-#        raise ValueError(
- #           f"Preflop lossless abstraction only works for a short deck with "
-  #          f"ranks [10, jack, queen, king, ace]. What was specified="
-   #         f"{found_ranks} doesn't equal what is allowed={allowed_ranks}"
-    #    )
+    allowed_ranks = {10, 11, 12, 13, 14}
+    found_ranks = set([c.rank_int for c in builder._cards])
+    if found_ranks != allowed_ranks:
+        raise ValueError(
+            f"Preflop lossless abstraction only works for a short deck with "
+            f"ranks [10, jack, queen, king, ace]. What was specified="
+            f"{found_ranks} doesn't equal what is allowed={allowed_ranks}"
+        )
     # Getting combos and indexing with lossless abstraction
     preflop_lossless: Dict[Tuple[Card, Card], int] = {}
     for starting_hand in builder.starting_hands:
@@ -46,5 +70,7 @@ def compute_preflop_lossless_abstraction(builder) -> Dict[Tuple[int, int], int]:
             key=operator.attrgetter("eval_card"),
             reverse=True
         )
-        preflop_lossless[tuple(starting_hand)] = rank_set(starting_hand)
+        preflop_lossless[tuple(starting_hand)] = make_starting_hand_lossless(
+            starting_hand, builder
+        )
     return preflop_lossless
