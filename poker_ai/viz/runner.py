60b54c4a3e0afef5f128ffe26337d8b4f32cb15a
bigcnote <bigcnote.protonmail.com>
2023-04-02 13:37:32 -0400

    document cli

--- poker_ai/viz/runner.py
+++ poker_ai/viz/runner.py
@@ -14,22 +14,27 @@ from flask import Flask, render_template
 
 
 log = logging.getLogger(__name__)
+
+# Flask settings for the viz.
 repo_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 template_folder = os.path.join(repo_root, "viz")
 app = Flask(__name__, template_folder=template_folder)
 
-# Will be cli args.
-betting_round_viz = "flop_hand_cluster_0_call-call-call"
-strategy_path = "../../_2023_04_01_18_29_31_951511/agent.joblib"
-
 
 class StrategySizeLevelDict:
+    """
+    Object used for storing the size and max level of a strategy for use
+    in the strategy viz.
+    """
+
     def __init__(self):
         self.size = defaultdict(int)
         self.max_level = 0
 
 
 class InfoSet(TypedDict):
+    """Info set for a given state. """
+
     cards_cluster: int
     betting_stage: str
     history: List[Dict[str, List[str]]]
@@ -41,8 +46,36 @@ def to_start_of_betting_round_str(info_set_str: str) -> Tuple[str, List[str]]:
     for use in the DAG vizualizations. Also return the history of actions for
     the current betting stage.
 
-    TODO: Doc string, show example for fast lookup of a particular betting
-    round.
+    Example Useage.
+    >>> strategy = joblib.load('path/to/strategy_dir/agent.joblib')
+    >>> strategy['strategy'].keys()
+    Make sure to user '' around the infoset so it can be parsed by your
+    shell.
+
+    Eg;
+    >>> info_set_str = '{
+    >>>    "cards_cluster":9,
+    >>>    "betting_stage":"turn",
+    >>>    "history":[
+    >>>        {"pre_flop":["raise","raise","raise","call","call"]},
+    >>>        {"flop":["raise","raise","fold","call"]},
+    >>>        {"turn":["raise","raise"]}
+    >>>    ]
+    >>> }'
+    >>> to_start_of_betting_round_str(info_set_str)
+
+    Params
+    ------
+    info_set_str : str
+        The info set string for a given state. As retrieved from the strategy.
+
+    Returns
+    -------
+    top_of_betting_round : str
+        The string representing the start of the betting round for the given state.
+        Roughly `{betting_stage}_card_cluster_{cluster}{action_history}`.
+    history : List[str]
+        The history of actions for the current betting stage.
     """
     # For understanding which betting stage is later than the another.
     comp_lookup: Dict[str, int] = {"pre_flop": 0, "flop": 1, "turn": 2, "river": 3}
@@ -68,15 +101,19 @@ def to_start_of_betting_round_str(info_set_str: str) -> Tuple[str, List[str]]:
             for actions in past_betting_stage_dict.values():
                 history.extend(actions)
     top_of_betting_round = f"{betting_stage}_hand_cluster_{cluster}{action_history}"
+
     return top_of_betting_round, history
 
 
-def _get_bot_dag_data(strategy_path: str) -> Dict[str, StrategySizeLevelDict]:
+def _get_bot_dag_data(strategy_path: Path) -> Dict[str, StrategySizeLevelDict]:
     """
     Loop over the strategy and create a lookup of stats from bot's state strategies.
     We will use this to create a DAG.
 
-    TODO
+    Params
+    ------
+    strategy_path : Path
+        The path to the strategy file.
     """
     bot_dag_data: Dict[str, StrategySizeLevelDict] = defaultdict(StrategySizeLevelDict)
     strategy = joblib.load(strategy_path)
@@ -96,6 +133,8 @@ def _get_bot_dag_data(strategy_path: str) -> Dict[str, StrategySizeLevelDict]:
             level = len(path) - 1
 
             # Get a lookup for a path at this state.
+            # Size is scale 0-1000, the strategy is converted to a probability then
+            # scaled to 0-1000.
             bot_dag_data[start_of_betting_round].size[os.path.join(*path)] = int(
                 1000 * probability / norm
             )
@@ -109,6 +148,19 @@ def _generate_action_combos(base_path, level):
     """
     Helper function to generate all possible action combinations for a given
     level of the DAG. Plus it eliminates invalid paths.
+
+    Params
+    ------
+    base_path : str
+        Usually this would be the `start_of_betting_round` string.
+    level : int
+        The level of the strategy as based on the start of the betting round.
+
+    Returns
+    -------
+    valid_paths : List[str]
+        A list of valid paths for the given level. Removes sequences of three
+        folds prior to the last action.
     """
     actions = ["fold", "raise", "call"]
     combinations = itertools.product(actions, repeat=level)
@@ -121,18 +173,28 @@ def _generate_action_combos(base_path, level):
     return valid_paths
 
 
-def _get_betting_round_action_combos(betting_round_viz, max_generate_level):
+def _get_betting_round_action_combos(betting_round_state_viz, max_generate_level):
     """
     For a given betting round, and the max level in the bot's strategy for that
     betting round, generate all possible paths for the betting round.
+
+    Params
+    ------
+    betting_round_state_viz : str
+        The string representing the start of the betting round for the given
+        state. This will be the root node of the vizualizations.
+    max_generate_level : int
+        The max level in the bot's strategy for the given betting round.
     """
     betting_round_action_combos: Dict[str, List[Any]] = defaultdict(list)
+    # Size is scale 0-1000, the strategy is converted to a probability then
+    # scaled to 0-1000.
     betting_round_action_combos["size"].append(1000)
-    betting_round_action_combos["path"].append(betting_round_viz)
+    betting_round_action_combos["path"].append(betting_round_state_viz)
     betting_round_action_combos["is_player"] = ["not_player"]
     # Get all possible paths for the current state.
     for level in range(1, max_generate_level + 1):
-        paths = _generate_action_combos(betting_round_viz, level)
+        paths = _generate_action_combos(betting_round_state_viz, level)
         betting_round_action_combos["path"].extend(paths)
         betting_round_action_combos["size"].extend([0] * len(paths))
         betting_round_action_combos["is_player"].extend(["not_player"] * len(paths))
@@ -140,24 +202,66 @@ def _get_betting_round_action_combos(betting_round_viz, max_generate_level):
 
 
 @click.command()
-@click.option("--strategy_dir", type=click.Path(exists=True))
-@click.option("--betting_round_viz", type=str)
-@click.option("--max_depth", type=int, default=7)
+@click.option(
+    "--strategy_dir",
+    type=click.Path(exists=True),
+    help="Path to the strategy directory.",
+)
+@click.option(
+    "--info_set_str",
+    type=str,
+    help=(
+        """
+        Infoset to vizualize, you can copy one from the output of the
+        strategy.
+
+        >>> strategy = joblib.load('path/to/strategy_dir/agent.joblib')
+        >>> strategy['strategy'].keys()
+        Make sure to user '' around the infoset so it can be parsed by your
+        shell.
+
+        Eg;
+        ```
+        '{
+            "cards_cluster":9,
+            "betting_stage":"turn",
+            "history":[
+                {"pre_flop":["raise","raise","raise","call","call"]},
+                {"flop":["raise","raise","fold","call"]},
+                {"turn":["raise","raise"]}
+            ]
+        }'
+        ```
+        """
+    ),
+)
+@click.option(
+    "--max_depth",
+    type=int,
+    default=7,
+    help="Max depth of the viz.  Recommend 7 or less, as the tree grows exponentially (~^3)",
+)
 @click.option("--host", default="127.0.0.1", help="The interface to bind to.")
-@click.option("--port", default=5000, help="The port to bind to.")
-def create_viz(strategy_dir, betting_round_viz, max_depth, host, port):
+@click.option("--port", default=8888, help="The port to bind to.")
+def create_viz(strategy_dir, info_set_str, max_depth, host, port):
     strategy_dir = Path(strategy_dir)
-    bot_dag_data = _get_bot_dag_data(strategy_dir / "agent.joblib")
-    max_generate_level = bot_dag_data[betting_round_viz].max_level
+    betting_round_state_viz, _ = to_start_of_betting_round_str(info_set_str)
+    bot_dag_data: Dict[str, StrategySizeLevelDict] = _get_bot_dag_data(
+        strategy_dir / "agent.joblib"
+    )
+    max_generate_level = bot_dag_data[betting_round_state_viz].max_level
     betting_round_action_combos = _get_betting_round_action_combos(
-        betting_round_viz, max_generate_level
+        betting_round_state_viz, max_generate_level
     )
 
+    # Update the size of the nodes based on the bot's strategy.
     for i, path in enumerate(betting_round_action_combos["path"]):
-        if path in bot_dag_data[betting_round_viz].size:
+        if path in bot_dag_data[betting_round_state_viz].size:
             betting_round_action_combos["size"][i] = bot_dag_data[
-                betting_round_viz
+                betting_round_state_viz
             ].size[path]
+            # Used for coloring the vizualization to see the states the bot has
+            # a strategy for.
             betting_round_action_combos["is_player"][i] = "player"
 
     strategy_viz_dataset_path = (
